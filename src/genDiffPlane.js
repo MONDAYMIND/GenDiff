b2fd33a25ed528f14b41abdac502a4aa7b350b68
Анна Стексова <anya-steksova@mail.ru>
2022-05-06 17:38:52 +1000

    refact main function and workflow

--- src/genDiffPlane.js
+++ src/genDiffPlane.js
@@ -12,26 +12,25 @@ const genDiffPlane = (filepath1, filepath2) => {
   if (!filepath1.endsWith('json') || !filepath2.endsWith('json')) {
     return 'Wrong format of file!';
   }
-  const data1 = readFileSync(formAbsolutePath(filepath1), 'utf8');
-  const data1obj = JSON.parse(data1);
-  const data2 = readFileSync(formAbsolutePath(filepath2), 'utf8');
-  const data2obj = JSON.parse(data2);
+  const file1obj = JSON.parse(readFileSync(formAbsolutePath(filepath1), 'utf8'));
+  const file2obj = JSON.parse(readFileSync(formAbsolutePath(filepath2), 'utf8'));
 
-  const entries1 = Object.entries(data1obj);
-  const entries2 = Object.entries(data2obj);
+  const entries1 = Object.entries(file1obj);
+  const entries2 = Object.entries(file2obj);
   const entries = _.unionWith(entries1, entries2, _.isEqual);
   const sortedEntries = _.sortBy(entries, ([a]) => a[0]);
 
   const lines = sortedEntries.map(([key, value]) => {
-    const keyValueString = `${key}: ${value}`;
-    if (!Object.hasOwn(data1obj, key)
-    || (data1obj[key] !== data2obj[key] && value === data2obj[key])) {
-      return `  + ${keyValueString}`;
-    } if (!Object.hasOwn(data2obj, key)
-    || (data1obj[key] !== data2obj[key] && value === data1obj[key])) {
-      return `  - ${keyValueString}`;
+    let operator = ' ';
+    const objectComparison = file1obj[key] === file2obj[key];
+    if (!Object.hasOwn(file1obj, key)
+    || (!objectComparison && value === file2obj[key])) {
+      operator = '+';
+    } if (!Object.hasOwn(file2obj, key)
+    || (!objectComparison && value === file1obj[key])) {
+      operator = '-';
     }
-    return `    ${keyValueString}`;
+    return `  ${operator} ${key}: ${value}`;
   });
 
   return [
